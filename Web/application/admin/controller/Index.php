5a87ddca3501e896eee0fd6a4a0af7c3e7e5719d
BoHongtao <BoHongtao@yeah.net>
2018-12-11 09:13:02 +0800

    每10min爬取一次

--- Web/application/admin/controller/Index.php
+++ Web/application/admin/controller/Index.php
@@ -13,17 +13,30 @@ class Index extends Controller
         'msg'=>''
     ];
     public function index(){
-         exec('python /home/code/spider.py');
-        if($this->request->isAjax()){
-            $data_dyj = Db::name('lottery_phoenix_tree')->order('id','desc')->limit(1)->select();
-            $data_ssq = Db::name('lottery_phoenix_tree_ssq')->order('id','open_time')->limit(1)->select();
-//            json_decode($data_dyj[0]["content"]);
-//            json_decode($data_ssq[0]);
-            $this->res['code'] = '200';
-            $this->res['data_ssq'] = $data_ssq[0];
-            $this->res['data_dyj'] = $data_dyj[0]["content"];
-            return $this->res;
-        }
         return $this->fetch();
+    }
+//    public function data(){
+//        if($this->request->isAjax()){
+//            $fn = $this->request->get('callback');
+//            $data_dyj = Db::name('lottery_phoenix_tree')->order('id','desc')->limit(1)->select();
+//            $data_ssq = Db::name('lottery_phoenix_tree_ssq')->order('id','open_time')->limit(1)->select();
+//            $this->res['code'] = '200';
+//            $this->res['data_ssq'] = $data_ssq[0];
+//            $this->res['data_dyj'] = $data_dyj[0]["content"];
+//            $arr = json_encode($this->res);
+//            return $fn."($arr)";
+//        }
+//    }
+    public function dyj(){
+        header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求
+        header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With');
+        if($this->request->isGet()){
+            $fn = $this->request->get('callback');
+            $data_dyj = Db('LotteryDyj')->limit(500)->select();
+            $this->res['code'] = '200';
+            $this->res['data_dyj'] = $data_dyj;
+            $arr = json_encode($this->res);
+            return $fn."($arr)";
+        }
     }
 }
---
+++ Web/application/admin/model/LotteryDyj.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: john
+ * Date: 2018/11/15
+ * Time: 21:33
+ */
+namespace app\admin\mode;
+use think\Model;
+
+class LotteryDyj extends Model
+{
+    protected $table = 'lottery_dyj';
+}
